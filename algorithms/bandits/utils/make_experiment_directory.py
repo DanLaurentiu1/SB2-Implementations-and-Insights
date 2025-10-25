127c696929a39c3e8c27390bea74c6ec2af33290
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-10-25 15:19:07 +0300

    chore: create placeholder utlis

---
+++ algorithms/bandits/utils/make_experiment_directory.py
@@ -0,0 +1,2 @@
+def make_experiment_directory(path: str, name: str):
+    print(f"make_experiment_directory received path:{path} and name:{name}")
---
+++ utils/logging/CSVLogger.py
@@ -0,0 +1,7 @@
+import csv
+from pathlib import Path
+
+
+class CSVLogger:
+    def __init__(self, run_dir):
+        print("Logger hit!")
