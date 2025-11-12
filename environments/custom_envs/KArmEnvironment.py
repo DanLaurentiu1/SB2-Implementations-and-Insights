971019df8ea9417323fc710df9ce34cec0054ed3
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-11-12 16:51:44 +0200

    feat(exception): implement logic validation in the simple bandit env (#13)

--- environments/custom_envs/KArmEnvironment.py
+++ environments/custom_envs/KArmEnvironment.py
@@ -3,16 +3,24 @@ from gymnasium.utils.seeding import np_random
 from gymnasium import Env
 from gymnasium.spaces import Discrete
 
+from utils.exceptions.logic_exceptions import EnvironmentLogicException
+
 
 class KArmEnvironment(Env):
     def __init__(self, number_of_arms: int = 10, seed: int = 16, max_steps: int = 1000):
+        self._validate_input(
+            number_of_arms=number_of_arms, seed=seed, max_steps=max_steps
+        )
+
+        self.terminated = False
+        self.truncated = False
         self.number_of_arms = number_of_arms
         self.seed = seed
         self.pulls = 0
         self.max_steps = max_steps
         self.np_random, _ = np_random(self.seed)
-        self._observation_space = Discrete(1, seed=self.seed)
-        self._action_space = Discrete(n=self.number_of_arms, seed=self.seed, start=0)
+        self.observation_space = Discrete(1, seed=self.seed)
+        self.action_space = Discrete(n=self.number_of_arms, seed=self.seed, start=0)
         self._get_new_arms()
 
     def _get_new_arms(self):
@@ -27,6 +35,32 @@ class KArmEnvironment(Env):
     def _get_info(self, optimal_arm_chosen: bool):
         return {"optimal_arm_chosen": optimal_arm_chosen}
 
+    def _ensure_not_terminated(self):
+        if self.terminated:
+            raise EnvironmentLogicException(
+                f"step() called after rollout termination. call reset() first."
+            )
+
+    def _validate_input(self, number_of_arms: int, seed: int, max_steps: int):
+        if number_of_arms < 1:
+            raise EnvironmentLogicException(
+                f"Invalid number of arms={number_of_arms}. This number must be positive and bigger than 0."
+            )
+        if max_steps < 1:
+            raise EnvironmentLogicException(
+                f"Invalid number of max_steps={max_steps}. This number must be positive and bigger than 0."
+            )
+        if seed < 0:
+            raise EnvironmentLogicException(
+                f"Invalid seed={seed}. This number must be positive."
+            )
+
+    def _validate_action(self, action: int):
+        if not (0 <= action < self.number_of_arms):
+            raise EnvironmentLogicException(
+                f"Invalid action={action}. Action must be a member of [0, {self.number_of_arms - 1}]"
+            )
+
     def reset(self, *, seed=None):
         super().reset(seed=seed)
         if seed is not None:
@@ -34,21 +68,24 @@ class KArmEnvironment(Env):
             self.np_random, _ = np_random(self.seed)
             self._get_new_arms()
 
+        self.terminated = False
         self.pulls = 0
         info = {"optimal_arm": self.optimal_arm}
         return self._get_obs(), info
 
     def step(self, action: int):
-        truncated = False
+        self._ensure_not_terminated()
+        self._validate_action(action=action)
+
         self.pulls += 1
-        terminated = self.pulls == self.max_steps
+        if self.pulls == self.max_steps:
+            self.terminated = True
         is_optimal = action == self.optimal_arm
-        # this works only if action is [0, self.number_of_arms - 1]
         reward = self.np_random.normal(loc=self.arm_means[action], scale=1.0)
         return (
             self._get_obs(),
             reward,
-            terminated,
-            truncated,
+            self.terminated,
+            self.truncated,
             self._get_info(optimal_arm_chosen=is_optimal),
         )
