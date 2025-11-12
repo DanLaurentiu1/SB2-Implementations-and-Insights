435bf4ae6b0df004da9aafdc74bf257a3e32605c
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-11-12 16:51:19 +0200

    feat(exception): create logic exception hierarchy

---
+++ utils/exceptions/logic_exceptions.py
@@ -0,0 +1,8 @@
+class LogicException(Exception):
+    def __init__(self, message: str):
+        super().__init__(f"[LogicException] {message}")
+
+
+class EnvironmentLogicException(LogicException):
+    def __init__(self, message: str):
+        super().__init__(f"[EnvironmentLogicException] {message}")
