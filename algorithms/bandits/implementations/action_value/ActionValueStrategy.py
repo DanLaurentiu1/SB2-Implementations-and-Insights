a380121f1ae1d089d59761ea92c13c3b3586c246
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-12-06 16:07:57 +0200

    feat!: implement action-value strategies (#26)

---
+++ algorithms/bandits/implementations/action_value/ActionValueStrategy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from environments.custom_envs.BanditEnvs import BaseBanditEnv
+
+
+class ActionValueStrategy(ABC):
+    def __init__(self, **kwargs):
+        pass
+
+    def setup(self, env: BaseBanditEnv) -> None:
+        pass
+
+    @abstractmethod
+    def update_action_value(
+        self, q_values: np.ndarray, action: int, reward: float
+    ) -> None:
+        pass
---
+++ algorithms/bandits/implementations/action_value/ERWAverageSampling.py
@@ -0,0 +1,16 @@
+import numpy as np
+from algorithms.bandits.implementations.action_value.ActionValueStrategy import (
+    ActionValueStrategy,
+)
+from utils.exceptions.logic_exceptions import ActionValueLogicException
+
+
+class ERWAverageSampling(ActionValueStrategy):
+    def __init__(self, alpha: float, **kwargs):
+        super().__init__(**kwargs)
+        self._alpha = alpha
+
+    def update_action_value(
+        self, q_values: np.ndarray, action: int, reward: float
+    ) -> None:
+        q_values[action] += self._alpha * (reward - q_values[action])
