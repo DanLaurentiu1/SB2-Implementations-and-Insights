7da2f6b0403fcddf815e920069c42cc3a5d272e1
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-12-06 16:04:04 +0200

    feat: add bandit strategies exceptions

--- utils/exceptions/logic_exceptions.py
+++ utils/exceptions/logic_exceptions.py
@@ -16,3 +16,13 @@ class DriftingLogicException(LogicException):
 class RewardLogicException(LogicException):
     def __init__(self, message: str):
         super().__init__(f"[RewardLogicException] {message}")
+
+
+class ExplorationLogicException(LogicException):
+    def __init__(self, message: str):
+        super().__init__(f"[ExplorationLogicException] {message}")
+
+
+class ActionValueLogicException(LogicException):
+    def __init__(self, message: str):
+        super().__init__(f"[ActionValueLogicException] {message}")
