65d5a6f8ea578f8e91cfb9418216e3130f6bf5b2
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-12-08 14:29:43 +0200

    refactor: refactor env + agent string representations

--- environments/custom_envs/BanditEnvs/KArmEnvironment.py
+++ environments/custom_envs/BanditEnvs/KArmEnvironment.py
@@ -163,7 +163,7 @@ class KArmEnvironment(Env, BaseBanditEnv):
 
     # removed reward strategy representation because of windows file length limit (!)
     def __str__(self):
-        return f"{self.__class__.__name__}(s={self.seed},dft={self._drift_stategy.__class__.__name__})"
+        return f"KArm(s={self.seed},dft={self._drift_stategy.__class__.__name__})"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(\n\tdrift={self._drift_stategy.__repr__()},\n\treward={self._reward_strategy.__repr__()},\n\tseed={self.seed},\n\tarms={self.number_of_arms}\n)"
