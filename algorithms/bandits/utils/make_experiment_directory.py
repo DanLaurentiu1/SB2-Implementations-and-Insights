898be8d0adaae2970c4a26c17b47ce4f081e13f4
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-12-08 13:24:13 +0200

    feat!: change experiment artifact dir structure (raw + processed)

--- algorithms/bandits/utils/make_experiment_directory.py
+++ algorithms/bandits/utils/make_experiment_directory.py
@@ -6,7 +6,10 @@ def make_experiment_directory(base_path: Path, name: str) -> Path:
     date_str = datetime.now().strftime("%d_%B_%Y")
     folder_name = f"{name}_{date_str}"
 
-    experiment_path = base_path / folder_name
+    processed_experiment_path = base_path / folder_name / "processed"
+    processed_experiment_path.mkdir(exist_ok=True, parents=True)
 
-    experiment_path.mkdir(exist_ok=True)
-    return experiment_path
+    raw_experiment_path = base_path / folder_name / "raw"
+    raw_experiment_path.mkdir(exist_ok=True, parents=True)
+
+    return raw_experiment_path
