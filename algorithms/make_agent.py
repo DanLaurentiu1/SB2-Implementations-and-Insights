135610c514111df3a9057f51633306205a1c820a
Dan Laurentiu <danlaurentiuwork@gmail.com>
2025-12-20 15:03:00 +0200

    chore: change gitignore and coverage ignored files

--- algorithms/make_agent.py
+++ algorithms/make_agent.py
@@ -4,8 +4,11 @@ from typing import Callable
 import hydra
 from omegaconf import DictConfig
 
-from algorithms.bandits.implementations.action_value.ActionValueStrategy import (
-    ActionValueStrategy,
+from algorithms.bandits.implementations.action_value_initialization.ActionValueInitializationStrategy import (
+    ActionValueInitializationStrategy,
+)
+from algorithms.bandits.implementations.action_value_update.ActionValueStrategy import (
+    ActionValueUpdateStrategy,
 )
 from algorithms.bandits.implementations.BaseBanditAgent import BaseBanditAgent
 from algorithms.bandits.implementations.exploration.ExplorationExploitationStrategy import (
@@ -16,7 +19,12 @@ from environments.custom_envs.BanditEnvs.BaseBanditEnv import BaseBanditEnv
 
 def make_factory(
     cfg: DictConfig,
-) -> Callable[..., ActionValueStrategy | ExplorationExploitationStrategy]:
+) -> Callable[
+    ...,
+    ActionValueUpdateStrategy
+    | ExplorationExploitationStrategy
+    | ActionValueInitializationStrategy,
+]:
     strategy_class = hydra.utils.get_class(cfg["_target_"])
     params = {k: v for k, v in cfg.items() if k != "_target_"}
     strategy_factory = partial(strategy_class, **params)
@@ -27,14 +35,20 @@ def make_agent(cfg: DictConfig, seed: int, env: BaseBanditEnv) -> BaseBanditAgen
     agent_target = cfg["_target_"]
     AgentClass = hydra.utils.get_class(agent_target)
     agent_params = cfg["params"]
-    action_factory = make_factory(agent_params["action_value_strategy"])
+    action_value_update_factory = make_factory(
+        agent_params["action_value_update_strategy"]
+    )
     exploration_factory = make_factory(agent_params["exploration_strategy"])
+    action_value_initialization_factory = make_factory(
+        agent_params["action_value_initialization_strategy"]
+    )
     metrics = agent_params["metrics"]
     agent = AgentClass(
         env=env,
         seed=seed,
         metrics=metrics,
-        action_value_factory=action_factory,
+        action_value_update_factory=action_value_update_factory,
         exploration_factory=exploration_factory,
+        action_value_initialization_factory=action_value_initialization_factory,
     )
     return agent
